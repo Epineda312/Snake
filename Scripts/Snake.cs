0fe8768f36cedc305ecf9b543ec96ec56e8edaf6
Airic <epineda773@protonmail.com>
2018-09-24 14:25:58 -0700

    Add files via upload
    Movement Switch Case

--- Scripts/Snake.cs
+++ Scripts/Snake.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Snake : MonoBehaviour
 {
     public GameObject snakePrefab;
-    public Snake Head;
-    public Snake Tail;
+    public Snake head;
+    public Snake tail;
     public int NESW;
-    public Vector2 newPos;
+    public Vector2 nextPos;
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +23,25 @@ public class Snake : MonoBehaviour
     void Movement()
     {
         GameObject temp;
+        nextPos = head.transform.position;
+
+        switch(NESW)
+        {
+            case 0:
+                nextPos = new Vector2(nextPos.x, nextPos.y + 1);
+                break;
+            case 1:
+                nextPos = new Vector2(nextPos.x + 1, nextPos.y);
+                break;
+            case 2:
+                nextPos = new Vector2(nextPos.x, nextPos.y - 1);
+                break;
+            case 3:
+                nextPos = new Vector2(nextPos.x -1, nextPos.y + 1);
+                break;
+
+
+        }
 
     }
 }
