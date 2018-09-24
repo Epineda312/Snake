339737d5504a3232d66d52bd81a4f8d590a19728
Airic <epineda773@protonmail.com>
2018-09-24 14:08:23 -0700

    Add files via upload
    Snake C# Scripts

---
+++ Scripts/Snake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    public GameObject snakePrefab;
+    public Snake Head;
+    public Snake Tail;
+    public int NESW;
+    public Vector2 newPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Movement()
+    {
+        GameObject temp;
+
+    }
+}
